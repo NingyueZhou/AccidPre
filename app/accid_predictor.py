ad0a7e2ace252e3f3ee281f7c2d7b5fc632b8c4b
Ningyue Zhou <ge52qoj@tum.de>
2023-12-04 22:38:21 +0100

    built endpoint with fastAPI

--- app/accid_predictor.py
+++ app/accid_predictor.py
@@ -1,14 +1,45 @@
-from neuralprophet import save, load
+import pandas as pd
+from datetime import datetime
+from neuralprophet import load
+
+def calc_periods(date):
+    
+    start = datetime(day=1,month=1,year=2000)
+    end = datetime(day=1,month=12,year=2020)
+    
+    if date < start:
+        periods = -1
+    elif start <= date <= end:
+        periods = 0
+    elif date > end:
+        periods = (date.year - end.year) * 12 + (date.month - end.month)
+    
+    return periods
 
 def predict_accid(year, month, category='alk', type='ins'):
     
+    date = datetime(year=year, month=month, day=1)
+    periods = calc_periods(date)
     
+    if periods == -1:
+        predicted_accid = -1
     
-    model = load('../models/neuralprophet/model_' + category + type +'.np')
-    
-    future = model.make_future_dataframe(periods=365)
-    forecast = m.predict(future)
-    
-    # round prediction to nearest integer!!!
-    
-    return forecast
+    elif periods == 0:
+        data = pd.read_csv('./data/data_' + category + '_' + type + '.csv')
+        print(data.loc[data.ds == date, 'y'].values)
+        predicted_accid = data.loc[data.ds == str(date)[:10], 'y']
+        
+    else:
+        data = pd.read_csv('./data/data_' + category + '_' + type + '.csv')
+        
+        model = load('./models/neuralprophet/model_' + category + '_' + type +'.np')
+        
+        future = model.make_future_dataframe(data, periods=periods, n_historic_predictions=len(data))
+        future.sort_values(by='ds', inplace=True, ascending=True)
+        future.reset_index(drop=True, inplace=True)
+        forecast = model.predict(future)
+        
+        predicted_accid = forecast['yhat1'].iloc[-1]
+        predicted_accid = round(predicted_accid)
+        
+    return predicted_accid
--- app/app.py
+++ app/app.py
@@ -1,3 +1,4 @@
+import uvicorn
 from fastapi import FastAPI
 from pydantic import BaseModel
 from accid_predictor import predict_accid
@@ -5,8 +6,8 @@ from accid_predictor import predict_accid
 app = FastAPI()
 
 class DateTimeIn(BaseModel):
-    year: str
-    month: str
+    year: int
+    month: int
 
 class PredictionOut(BaseModel):
     prediction: int
@@ -16,6 +17,10 @@ def index():
     return {"Heathcheck": "OK"}
 
 @app.post("/predict", response_model=PredictionOut)
-def predict(payload: DateTimeIn):
+async def predict_endpoint(payload: DateTimeIn):
     predicted_accid = predict_accid(year=payload.year, month=payload.month)
     return {"prediction": predicted_accid}
+
+if __name__ == '__main__':
+    uvicorn.run(app, host='127.0.0.1', port=4343)
+#uvicorn app:app --reload
