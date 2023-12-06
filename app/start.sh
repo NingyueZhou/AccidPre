a151a8370cac4df10ae34ed904b63c07ed499d1d
Ningyue Zhou <ge52qoj@tum.de>
2023-12-06 14:45:20 +0100

    updated start.sh

--- app/start.sh
+++ app/start.sh
@@ -3,4 +3,7 @@
 cd "$(dirname "$0")"
 #echo "$(pwd)"
 
-uvicorn app:app --reload --host 0.0.0.0 --log-level "info"
+sudo service nginx restart
+
+uvicorn app:app --reload --host 0.0.0.0 --log-level "info" -uds=/tmp/uvicorn.sock
+#python3 -m uvicorn app:app --reload --host 0.0.0.0 --log-level "info"
