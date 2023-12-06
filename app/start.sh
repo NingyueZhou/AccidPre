5f1b2097df18fcdde26ce1376cb688b649d94a07
Ningyue Zhou <ge52qoj@tum.de>
2023-12-06 17:58:38 +0100

    update start.sh

--- app/start.sh
+++ app/start.sh
@@ -7,4 +7,4 @@ sudo service nginx restart
 
 #python3 -m uvicorn app:app --reload --host 0.0.0.0 --log-level "info"
 #uvicorn app:app --reload --host 0.0.0.0 --log-level info --forwarded-allow-ips='*' --uds='/tmp/uvicorn.sock' --proxy-headers
-uvicorn app:app --reload --host 0.0.0.0 --log-level info --forwarded-allow-ips='*' --proxy-headers
+python3 -m uvicorn app:app --reload --host 0.0.0.0 --log-level info --forwarded-allow-ips='*' --proxy-headers
